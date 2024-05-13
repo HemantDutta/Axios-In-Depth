65881ae5a001d5d7e3cc5d05411c737a83508e85
Hemant Dutta <hemantnorddutta2@gmail.com>
2024-05-13 16:27:11 +0530

    Get Request

--- app/src/components/1-first-request.js
+++ app/src/components/1-first-request.js
@@ -1,12 +1,27 @@
 import { useEffect } from 'react';
+import axios from "axios";
 
-// limit, if 429 wait for 15 min and try again
-const url = 'https://course-api.com/react-store-products';
+const url = 'https://randomuser.me/pi/';
 
 const FirstRequest = () => {
+
+  const fetchData = async () => {
+    try {
+      const response = await axios(url);
+      console.log(response);
+    }
+    catch (error) {
+      console.log(error.response);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
     <>
-      <h2 className='text-center'>first request</h2>
+      <div className="text-center p-5 font-semibold bg-black text-white text-2xl">First Request</div>
     </>
   );
 };
