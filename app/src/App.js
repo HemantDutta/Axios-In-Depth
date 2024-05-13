646e1febc29777437f6b464d053b8585eac17b43
Hemant Dutta <hemantnorddutta2@gmail.com>
2024-05-13 16:43:03 +0530

    Post Request

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import FirstRequest from "./components/1-first-request";
 import Headers from "./components/2-headers";
+import PostRequest from "./components/3-post-request";
 
 function App() {
   return (
     <>
       <FirstRequest />
       <Headers />
+      <PostRequest/>
     </>
   )
 }
--- app/src/components/3-post-request.js
+++ app/src/components/3-post-request.js
@@ -1,46 +1,53 @@
 import { useState } from 'react';
 import axios from 'axios';
+
 const url = 'https://course-api.com/axios-tutorial-post';
 
 const PostRequest = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(name, email);
+    try {
+        const response = await axios.post(url, {name, email});
+        console.log(response.data);
+    }
+    catch (error) {
+      console.log(error.response);
+    }
   };
 
   return (
-    <section>
-      <h2 className='text-center'>post request</h2>
-      <form className='form' onSubmit={handleSubmit}>
-        <div className='form-row'>
+    <section className="p-5 bg-blue-500 flex flex-col gap-5">
+      <span className="text-center text-2xl text-white">Post Request</span>
+      <form className="form bg-white p-2 rounded flex flex-col items-center gap-4" onSubmit={handleSubmit}>
+        <div className='form-row flex flex-col gap-1 w-full'>
           <label htmlFor='name' className='form-label'>
-            name
+            Name
           </label>
           <input
             type='text'
-            className='form-input'
+            className='form-input border border-gray-500 rounded'
             id='name'
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
         </div>
-        <div className='form-row'>
+        <div className='form-row flex flex-col gap-1 w-full'>
           <label htmlFor='email' className='form-label'>
-            email
+            Email
           </label>
           <input
             type='email'
-            className='form-input'
+            className='form-input border border-gray-500 rounded'
             id='email'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <button type='submit' className='btn btn-block'>
-          login
+        <button type='submit' className='py-2 px-4 bg-blue-500 w-max rounded text-white hover:bg-blue-200 hover:text-black'>
+          Login
         </button>
       </form>
     </section>
